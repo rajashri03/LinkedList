0f54a182ea65a9a30093a152a2f27ff5224eb15f
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-09 14:36:26 +0530

    Creating Linked List

---
+++ LinkedList/LinkedList/AddNode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlist
+{
+    public class AddNode<link>//Created Generic class
+    {
+        public link data;
+        public AddNode<link> next;
+        public AddNode(link value)//Constructor
+        {
+            data = value;
+        }
+    }
+    public class Linked_List<link>
+    {
+        //Represent the head the  linked list  
+        public AddNode<link> head;
+        //Add() will add a new node to the list
+        public void Add(link data)
+        {
+            //Create a new node  
+            AddNode<link> node = new AddNode<link>(data);
+            //Checks if the list is empty  
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                AddNode<link> tempnode = head;
+                while (tempnode.next != null)
+                {
+                    tempnode = tempnode.next;
+                }
+                tempnode.next = node;
+            }
+            
+        }
+        //display method
+        public void Display()
+        {
+            if (head == null)
+                Console.WriteLine("The list is empty.");
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("List Contains: ");
+                Console.ResetColor();
+                AddNode<link> tempnode = head;
+                while (tempnode != null)
+                {
+                    Console.Write(tempnode.data + " ");
+                    tempnode = tempnode.next;
+                }
+            }
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿using Linkedlist;
+Console.Write("Select Number:\n1)Add elements");
+int option = Convert.ToInt32(Console.ReadLine());
+switch (option)
+{
+    case 1:
+        Linked_List<int> list = new Linked_List<int>();//creating list
+        //Adding element to the list  
+        list.Add(56);
+        list.Add(30);
+        list.Add(70);
+        //Display()- display the element in list
+        list.Display();
+        break;
+}
